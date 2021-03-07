4fadfac59ff485319d7e8cfb80dcac7c27a66278
Elena Slyusareva <sportgirl55@bk.ru>
2021-03-08 02:43:25 +0500

    Layout-2 is almost done

---
+++ 02-tajam/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header__nav');
+const menuItem = document.querySelectorAll('.header__menu li');
+
+burger.addEventListener('click', function () {
+  document.querySelector('.burger span').classList.toggle('active');
+  menu.classList.toggle('open');
+})
