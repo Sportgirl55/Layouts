4379077a22a7a461101aee106370d51714140136
Elena Slyusareva <sportgirl55@bk.ru>
2021-03-25 01:30:44 +0500

    Layout-5 is ready to inspection

--- 05/js/script.js
+++ 05/js/script.js
@@ -4,27 +4,28 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav');
 const menuItem = document.querySelectorAll('.header__menu li');
-
 const logo = document.querySelector('.header__logo');
-
-
-
 const search = document.querySelector('.header__search');
 
 
+burger.addEventListener('click', () => {
+  document.querySelector('.burger span').classList.toggle('active');
+  menu.classList.toggle("open");
+  logo.classList.toggle('up');
+  if (document.documentElement.clientWidth < 900) {
+      search.classList.toggle("visually-hidden");
+    }
 
+})
 
-burger.addEventListener('click', function(){
-  
-    document.querySelector('.burger span').classList.toggle('active');
-    search.classList.toggle("open");
-    menu.classList.toggle("open");
-    logo.classList.toggle('up');
-
-  })
-
- 
-  // menuItem.forEach(function (item) {
-  //   item.addEventListener('click', function () {
-  //     document.querySelector('.burger span').classList.toggle('active');
-  //   })
+menu.addEventListener('click', () => {
+  menu.classList.toggle("open");
+  document.querySelector('.burger span').classList.toggle('active');
+  logo.classList.toggle('up');
+  if (document.documentElement.clientWidth < 900) {
+    search.classList.toggle("visually-hidden");
+  }
+  if (document.documentElement.clientWidth > 1100) {
+    logo.classList.add('up');
+  }
+})
