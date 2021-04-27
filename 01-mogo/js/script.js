87b7f753c7d86504e7ba64a182791461c6d762ca
Elena Slyusareva <sportgirl55@bk.ru>
2021-04-28 00:43:48 +0500

    Layout-1 is ready to check once again

--- 01-mogo/js/script.js
+++ 01-mogo/js/script.js
@@ -2,21 +2,18 @@
 
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.header__nav-wrap');
-const menuItem = document.querySelectorAll('.header__menu li');
+const header = document.querySelector('.header');
 
-// const controls = document.querySelector('.controls');
-
-
-
-// // const search = document.querySelector('.header__search');
+const search = document.querySelector('.controls__search');
 
 
+burger.addEventListener('click', function () {
+    document.querySelector('.burger span').classList.toggle('active');
+    header.classList.toggle('open');
+    menu.classList.toggle('open')
+})
 
 
- burger.addEventListener('click', function() {
-     document.querySelector('.burger span').classList.toggle('active');
-    
-     menu.classList.toggle('open');
-//     // logo.classList.toggle('up');
-//  controls.classList.toggle("open");
-   })
+search.addEventListener('click', () => {
+    input.classList.add('open');
+})
