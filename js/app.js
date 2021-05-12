a9aaaf1ac4b60246b549cbf11c095ed9b11d69b3
Elena Slyusareva <sportgirl55@bk.ru>
2021-05-12 18:56:35 +0500

    Layout-4 is ready to inspection

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-console.log('Hello? world');
 
-import './css/style.css';
+'use strict';
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header__menu');
+
+
+burger.addEventListener('click', function () {
+  document.querySelector('.burger span').classList.toggle('active');
+  menu.classList.toggle('open');
+})
+
